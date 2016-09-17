cf5b5e216f41295ddcd3aadc32e93d7788f9ac70
velopert <public.velopert@gmail.com>
2016-09-17 23:50:59 +0900

    secretize passport keys

--- whotalk-backend/src/passport/fb.js
+++ whotalk-backend/src/passport/fb.js
@@ -1,6 +1,6 @@
 module.exports = {
   'appID' : '2114304178795129',
-  'appSecret' : 'a467439827e1bd57838ed890b26b22c2',
+  'appSecret' : process.env.FACEBOOK_SECRET,
   'callbackURL' : 'http://localhost:4000/api/authentication/facebook/callback',
-  'profileFields': ['id', 'name', 'emails', 'friends', 'gender', 'age_range']
+  'profileFields': ['id', 'name', 'emails', 'friends']
 }
