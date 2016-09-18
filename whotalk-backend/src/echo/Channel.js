b0df40bba455ebd325c082ab0cef3daaa24dc108
velopert <public.velopert@gmail.com>
2016-09-18 13:44:47 +0900

    Code Rewrite

---
+++ whotalk-backend/src/echo/Channel.js
@@ -0,0 +1,49 @@
+import _ from 'lodash';
+
+let sockets = null;
+const channels = {};
+
+
+function emit(connection, data) {
+    connection.write(JSON.stringify(data));
+}
+
+function Channel(name) {
+    this.name = name;
+    this.users = []; // stores userId
+
+    // adds userId
+    this.push = (userId) => {
+        this.users.push(userId);
+    };
+
+    // removes userId
+    this.remove = (userId) => {
+        _.remove(this.users, n => {
+            return n === userId;
+        });
+    };
+
+    //broadcast the data to this Channel
+    this.broadcast = (data) => {
+
+    }
+}
+
+function channel(_sockets) {
+    sockets = _sockets;
+}
+
+channel.create = (name) => {
+    channels[name] = new Channel(name);
+}
+
+channel.remove = (name) => {
+    delete channels[name];
+}
+
+channel.get = (name) => {
+    return channels[name];
+}
+
+export default channel;
--- whotalk-backend/src/routes/authentication.js
+++ whotalk-backend/src/routes/authentication.js
@@ -9,6 +9,7 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
    res.json({
+       sID: req.sessionID,
        session: req.session
    });
 });
