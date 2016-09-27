5080f85bc0eeb49fbd63cd4ffaedb47b0fb23ea8
velopert <public.velopert@gmail.com>
2016-09-27 11:21:10 +0000

    created ActionTypes

--- whotalk-frontend/src/helpers/request-action.js
+++ whotalk-frontend/src/helpers/request-action.js
@@ -1,14 +1,14 @@
 /*
-    Creates $NAME_REQUEST, $NAME_SUCESS, $NAME_FAILURE actions
+    Creates $NAME_INIT, $NAME_SUCESS, $NAME_FAILURE actions
     accessed by $NAME.$STATE, e.g. FETCH_MESSAGE.REQUEST
 */
 
-const REQUEST = 'REQUEST'
+const INIT = 'INIT'
 const SUCCESS = 'SUCCESS'
 const FAILURE = 'FAILURE'
 
 export default function createRequestTypes(base) {
-  return [REQUEST, SUCCESS, FAILURE].reduce((acc, type) => {
+  return [INIT, SUCCESS, FAILURE].reduce((acc, type) => {
 		acc[type] = `${base}_${type}`
 		return acc
 	}, {})
