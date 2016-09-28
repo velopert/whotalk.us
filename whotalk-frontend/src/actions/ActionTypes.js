53cd42035afa190848979b2fcbb51c6e51a95a16
velopert <public.velopert@gmail.com>
2016-09-28 19:48:02 +0900

    .envcpy move

--- whotalk-frontend/src/actions/ActionTypes.js
+++ whotalk-frontend/src/actions/ActionTypes.js
@@ -3,15 +3,15 @@ import { createRequestTypes } from 'utils';
 
 /* AUTHENTICATION */
 
-export CHECK_USERNAME_EXISTS_REQ = createRequestTypes('CHECK_USERNAME_EXISTS');
+export const CHECK_USERNAME_EXISTS_REQ = createRequestTypes('CHECK_USERNAME_EXISTS');
 
-export LOCAL_REGISTER_FIRST_STEP = 'LOCAL_REGISTER_FIRST_STEP';
-export LOCAL_REGISTER_REQ = createRequestTypes('LOCAL_REGISTER_REQ');
+export const LOCAL_REGISTER_FIRST_STEP = 'LOCAL_REGISTER_FIRST_STEP';
+export const LOCAL_REGISTER_REQ = createRequestTypes('LOCAL_REGISTER_REQ');
 
-export LOCAL_LOGIN_REQ = createRequestTypes('LOCAL_LOGIN_REQ');
+export const LOCAL_LOGIN_REQ = createRequestTypes('LOCAL_LOGIN_REQ');
 
-export OAUTH_CHECK_REQ = createRequestTypes('OAUTH_CHECK_REQ');
-export OAUTH_REGISTER_REQ = createRequestTypes('OAUTH_REGISTER_REQ')
+export const OAUTH_CHECK_REQ = createRequestTypes('OAUTH_CHECK_REQ');
+export const OAUTH_REGISTER_REQ = createRequestTypes('OAUTH_REGISTER_REQ')
 
 // This will be removed when server side rendering gets implemented
-export CHECK_SESSION_REQ createRequestTypes('CHECK_SESSION_REQ');
+export const CHECK_SESSION_REQ = createRequestTypes('CHECK_SESSION_REQ');
