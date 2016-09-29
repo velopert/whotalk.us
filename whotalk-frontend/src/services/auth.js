316673ac6d46a4373c2b1c7c514742cfd6f29437
velopert <public.velopert@gmail.com>
2016-09-29 20:12:49 +0900

    .

--- whotalk-frontend/src/services/auth.js
+++ whotalk-frontend/src/services/auth.js
@@ -3,3 +3,7 @@ import request from 'utils/request';
 export const checkUsername = (username) => {
     return request('/api/authentication/exists/velopert');
 }
+
+export const localRegister = ({username, password, familyName, givenName, gender, email}) => {
+    return request('/api/authentication/register')
+}
