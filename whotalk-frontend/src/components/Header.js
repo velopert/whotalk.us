87ef0a5df03c7fc613eab1902fc51b9aeda38f70
velopert <public.velopert@gmail.com>
2016-10-10 01:38:07 +0900

    menu icon animation

--- whotalk-frontend/src/components/Header.js
+++ whotalk-frontend/src/components/Header.js
@@ -1,5 +1,5 @@
-import React, { Component, PropTypes } from 'react';
-import { Link } from 'react-router';
+import React, {Component, PropTypes} from 'react';
+import {Link} from 'react-router';
 
 export const HeaderMode = {
     DEFAULT: 'DEFAULT',
@@ -22,12 +22,11 @@ const defaultProps = {
 
 class Header extends Component {
     render() {
-        
+
         const search = (
             <div className="search-button">
                 <div className="icon-wrapper">
-                    <i className="search icon">
-                    </i>
+                    <i className="search icon"></i>
                 </div>
             </div>
         )
@@ -35,13 +34,21 @@ class Header extends Component {
         return (
             <div className="header">
                 <div className="top">
-                    <div className={"bar" + (this.props.bar ? ' show' : '') }>
-                    </div>
+                    <div
+                        className={"bar" + (this.props.bar
+                        ? ' show'
+                        : '')}></div>
                     <Link to="/" className="top-logo">WHOTALK</Link>
-                    <div className="menu-icon"><i className="sidebar icon"></i></div>
-                    {this.props.search ? search : ''}
-                    
-                    
+                    <div className="menu-button">
+                        <div className="icon-wrapper">
+                            <i className="sidebar icon"></i>
+                        </div>
+                    </div>
+
+                    {this.props.search
+                        ? search
+                        : ''}
+
                 </div>
             </div>
         );
