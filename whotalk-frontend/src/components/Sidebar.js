99340ee9a393fd84a1372c330b0056549967baac
velopert <public.velopert@gmail.com>
2016-10-12 00:09:18 +0900

    sidebar animation fix

--- whotalk-frontend/src/components/Sidebar.js
+++ whotalk-frontend/src/components/Sidebar.js
@@ -2,10 +2,22 @@ import React from 'react';
 
 const Sidebar = ({open}) => {
     return (
-        <div className={`sidebar ${open?'open':''}`}>
-            
+        <div className={`sidebar ${open
+            ? 'open'
+            : ''}`}>
+  <a href="#">About</a>
+  <a href="#">Services</a>
+  <a href="#">Clients</a>
+  <a href="#">Contact</a>
+            <SignOut/>
         </div>
     );
 };
+
+const SignOut = ({onClick}) => (
+    <div className="sign-out">
+        
+    </div>
+)
 
 export default Sidebar;
