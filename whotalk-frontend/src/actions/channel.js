d1eb278e41d9c76429a43ebdd8a872bf222e03d5
velopert <public.velopert@gmail.com>
2016-10-26 04:18:27 +0900

    initialize action in channel

--- whotalk-frontend/src/actions/channel.js
+++ whotalk-frontend/src/actions/channel.js
@@ -2,6 +2,9 @@ import CHANNEL from './ActionTypes/channel';
 import { createAction } from 'redux-actions';
 import * as service from 'services/channel';
 
+
+export const initialize = createAction(CHANNEL.INITIALIZE);
+export const setIdentity = createAction(CHANNEL.SET_IDENTITY);
 export const checkValidity = (username) => ({
     type: CHANNEL.CHECK_VALIDITY,
     payload: {
