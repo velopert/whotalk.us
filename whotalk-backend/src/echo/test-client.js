3854f4bc4366a4797aca320e808ba82aa6f381cd
velopert <public.velopert@gmail.com>
2016-10-27 05:46:47 +0900

    client implementation

--- whotalk-backend/src/echo/test-client.js
+++ whotalk-backend/src/echo/test-client.js
@@ -3,39 +3,25 @@ import SockJS from 'sockjs-client';
 let intervalId = null;
 let socket = null;
 
+import {client as SEND} from './packetTypes';
 
-const TYPE = {
-    MSG: "MSG",
-    JOIN: "JOIN",
-    ERROR: "ERROR",
-    LEAVE: "LEAVE",
-    AUTH: "AUTH"
-};
-
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-})
+const rl = readline.createInterface({input: process.stdin, output: process.stdout})
 
 function ask(question) {
     return new Promise((resolve) => {
-        rl.question("[CLIENT] " + question + '\n> ', (answer) => { resolve(answer) })
+        rl.question("[CLIENT] " + question + '\n', (answer) => {
+            resolve(answer)
+        })
     });
 }
 
-function createMessage(message) {
-    return JSON.stringify({
-        type: "MSG",
-        message
-    });
+function generateUID() {
+    return (new Date().valueOf()).toString(36) + ("000" + (Math.random() * Math.pow(36, 3) << 0).toString(36)).slice(-3);
 }
 
 // creates action object
 function createAction(type, payload) {
-    return {
-        type,
-        payload
-    };
+    return {type, payload};
 }
 
 // creates action object
@@ -43,45 +29,30 @@ function send(data) {
     socket.send(JSON.stringify(data));
 }
 
+async function start() {
+    let channel = await ask("Channel?");
+    send(createAction(SEND.ENTER, {channel}));
 
-async function authenticate() {
-    let anon = await ask("Anonymously? y/n");
-    let session = null;
-    if(anon === "n") {
-        session = await ask("Input your sessionId");
-    } else {
-        session = (new Date()).toString();
+    const sessionID = (new Date()).toString(); //"dHkxl0zudajC19164Yqx2lQTJC94LEcN";
+
+    let anonymous = await ask("Anonymous? y/n");
+    if(anonymous === 'y') {
+        send(createAction(SEND.AUTH, {
+            sessionID,
+            anonymous: true
+        }));
     }
-    
-    let channel = await ask("Channel Name?");
-    const action = createAction(
-        TYPE.AUTH,
-        {
-            session,
-            channel,
-            anon: anon === 'y'
-        }
-    );
-    send(action);
 }
 
-// async function setUsername() {
-//     if (!username) {
-//         const name = await ask("[Client] What is your username?");
-//         username = name;
-//     }
-
-//     if (!channel) {
-//         const c = await ask("[Client] Type channel name to join");
-//         channel = c;
-//     }
-
-//     socket.send(createAction('JOIN', { username, channel }));
-// }
+// async function setUsername() {     if (!username) {         const name =
+// await ask("[Client] What is your username?");         username = name;     }
+//    if (!channel) {         const c = await ask("[Client] Type channel name to
+// join");         channel = c;     }     socket.send(createAction('JOIN', {
+// username, channel })); }
 
 function authenticate(session) {
-    // later-fix: should get the req.sessionID instead of data
-    const data = new Date();
+    // later-fix: should get the req.sessionID instead of data const data = new
+    // Date();
 
 }
 
@@ -90,7 +61,7 @@ const initConnection = () => {
     clearInterval(intervalId);
     socket.onopen = function () {
         console.log('connected');
-        authenticate();
+        start();
     };
     socket.onmessage = function (e) {
         console.log(e.data);
@@ -106,57 +77,31 @@ const initConnection = () => {
 
 initConnection();
 
-
-// const sock = new SockJS("http://localhost:4000/echo");
-
-//  sock.onopen = function() {
-//      console.log('connected');
-//  };
-//  sock.onmessage = function(e) {
-//      console.log('message', e.data);
-//  };
-//  sock.onclose = function() {
-//      console.log('close');
-//  };
-
-
-
-
-// async function initialize() {
-//     const name = await ask("[Client] What is your username?");
-//     console.log("[Client] Welcome, " + name);
-// }
+// const sock = new SockJS("http://localhost:4000/echo");  sock.onopen =
+// function() {      console.log('connected');  };  sock.onmessage = function(e)
+// {      console.log('message', e.data);  };  sock.onclose = function() {
+// console.log('close');  }; async function initialize() {     const name =
+// await ask("[Client] What is your username?");     console.log("[Client]
+// Welcome, " + name); }
 
 rl.setPrompt('');
 rl.prompt();
 
 rl.on('line', function (line) {
-    // const token = l.split(" ");
-    // if (token.length === 0) {
-    //     console.log('Invalid Request');
-    //     rl.prompt();
-    //     return;
-    // }
-
-    // const text = l.substring(4, l.length);
-    // switch (token[0]) {
-    //     case 'say':
-    //         send(createAction(TYPE.MSG, {
-    //             message: text
-    //         }));
-    //         break;
-    //     case 'send':
-    //         socket.send(text);
-    //         break;
-    //     default:
-    //         console.log('Invalid Request');
-    //         break;
-    // }
-    send(createAction(TYPE.MSG, {
-        message: line
+    // const token = l.split(" "); if (token.length === 0) {
+    // console.log('Invalid Request');     rl.prompt();     return; } const text =
+    // l.substring(4, l.length); switch (token[0]) {     case 'say':
+    // send(createAction(TYPE.MSG, {             message: text         }));
+    // break;     case 'send':         socket.send(text);         break;
+    // default:         console.log('Invalid Request');         break; }
+    send(createAction(SEND.MSG, {
+        message: line,
+        uID: generateUID()
     }));
+    
     rl.prompt();
-}).on('close', function () {
-    console.log('Have a great day!');
-    process.exit(0);
-});
+})
+    .on('close', function () {
+        console.log('Have a great day!');
+        process.exit(0);
+    });
