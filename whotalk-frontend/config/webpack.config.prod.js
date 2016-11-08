c92f393f22e1ee23813efec43dbe378c0c432765
velopert <public.velopert@gmail.com>
2016-11-08 20:48:16 +0900

    build script scss fix

--- whotalk-frontend/config/webpack.config.prod.js
+++ whotalk-frontend/config/webpack.config.prod.js
@@ -126,7 +126,7 @@ module.exports = {
       {
         test: /\.scss$/,
         include: [paths.appSrc, paths.appNodeModules],
-        loader: ExtractTextPlugin.extract('style', 'css?-autoprefixer!postcss')
+        loader: ExtractTextPlugin.extract('style', 'css?-autoprefixer!sass')
       },
       // JSON is not enabled by default in Webpack but both Node and Browserify
       // allow it implicitly so we also enable it.
