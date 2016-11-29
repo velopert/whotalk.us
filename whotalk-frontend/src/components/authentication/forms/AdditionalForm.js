d90cdf606c34a742176169c66d8b3f54d0f8d8a9
velopert <public.velopert@gmail.com>
2016-11-29 19:12:20 +0900

    done: change lang-based name order

--- whotalk-frontend/src/components/authentication/forms/AdditionalForm.js
+++ whotalk-frontend/src/components/authentication/forms/AdditionalForm.js
@@ -61,101 +61,114 @@ const AdditionalForm = ({
     onCancel,
     error,
     intl: {
+        locale,
         formatMessage
     }
-}) => (
-    <div className="ui massive form">
-        <div className="field">
-            <label>{formatMessage(messages.name)}</label>
-            <div className="two fields">
-                <div
-                    className={`field ${error.firstName
-                    ? 'error'
-                    : ''}`}>
-                    <input
-                        name="firstName"
-                        type="text"
-                        placeholder={formatMessage(messages.firstName)}
-                        onChange={onChange}
-                        value={form.firstName}/>
-                </div>
-                <div
-                    className={`field ${error.lastName
-                    ? 'error'
-                    : ''}`}>
-                    <input
-                        name="lastName"
-                        type="text"
-                        placeholder={formatMessage(messages.lastName)}
-                        onChange={onChange}
-                        value={form.lastName}/>
-                </div>
-            </div>
-        </div>
-        <div className={`field ${error.email
+}) => {
+
+    const firstName = (
+        <div
+            className={`field ${error.firstName
             ? 'error'
             : ''}`}>
-            <label>{formatMessage(messages.emailCapitalized)}</label>
-            <div
-                className={`ui left icon input ${status.checking
-                ? 'loading'
+            <input
+                name="firstName"
+                type="text"
+                placeholder={formatMessage(messages.firstName)}
+                onChange={onChange}
+                value={form.firstName}/>
+        </div>
+    )
+
+    const lastName = (
+        <div
+            className={`field ${error.lastName
+            ? 'error'
+            : ''}`}>
+            <input
+                name="lastName"
+                type="text"
+                placeholder={formatMessage(messages.lastName)}
+                onChange={onChange}
+                value={form.lastName}/>
+        </div>
+    )
+
+
+    return (
+        <div className="ui massive form">
+            <div className="field">
+                <label>{formatMessage(messages.name)}</label>
+                <div className="two fields">
+                     {locale=== 'ko' ? lastName : firstName}
+                     {locale=== 'ko' ? firstName : lastName}
+                </div>
+            </div>
+            <div className={`field ${error.email
+                ? 'error'
                 : ''}`}>
-                <input
-                    name="email"
-                    type="text"
-                    placeholder={formatMessage(messages.email)}
-                    onChange={onChange}
-                    value={form.email}
-                    onBlur={onBlur}/>
-                <i className="mail icon"></i>
+                <label>{formatMessage(messages.emailCapitalized)}</label>
+                <div
+                    className={`ui left icon input ${status.checking
+                    ? 'loading'
+                    : ''}`}>
+                    <input
+                        name="email"
+                        type="text"
+                        placeholder={formatMessage(messages.email)}
+                        onChange={onChange}
+                        value={form.email}
+                        onBlur={onBlur}/>
+                    <i className="mail icon"></i>
+                </div>
             </div>
-        </div>
-        <div className={`field ${error.gender
-            ? 'error'
-            : ''}`}>
-            <label>{formatMessage(messages.genderCapitalized)}</label>
-            <div className="ui selection dropdown">
-                <input type="hidden" name="gender" value={form.gender}/>
-                <i className="dropdown icon"></i>
-                <div className="default text">{formatMessage(messages.gender)}</div>
-                <div className="menu">
-                    <div
-                        className="item select-item"
-                        data-value="male"
-                        data-text={formatMessage(messages.male)}
-                        onClick={() => onSelect('gender', 'male')}>
-                        <i className="male icon"></i>
-                        {formatMessage(messages.male)}
-                    </div>
-                    <div
-                        className="item select-item"
-                        data-value="female"
-                        data-text={formatMessage(messages.female)}
-                        onClick={() => onSelect('gender', 'female')}>
-                        <i className="female icon"></i>
-                        {formatMessage(messages.female)}
+            <div className={`field ${error.gender
+                ? 'error'
+                : ''}`}>
+                <label>{formatMessage(messages.genderCapitalized)}</label>
+                <div className="ui selection dropdown">
+                    <input type="hidden" name="gender" value={form.gender}/>
+                    <i className="dropdown icon"></i>
+                    <div className="default text">{formatMessage(messages.gender)}</div>
+                    <div className="menu">
+                        <div
+                            className="item select-item"
+                            data-value="male"
+                            data-text={formatMessage(messages.male)}
+                            onClick={() => onSelect('gender', 'male')}>
+                            <i className="male icon"></i>
+                            {formatMessage(messages.male)}
+                        </div>
+                        <div
+                            className="item select-item"
+                            data-value="female"
+                            data-text={formatMessage(messages.female)}
+                            onClick={() => onSelect('gender', 'female')}>
+                            <i className="female icon"></i>
+                            {formatMessage(messages.female)}
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div className="ui grid">
-            <div className="six wide column">
-                <a className="massive ui button" onClick={onCancel}>
-                    {formatMessage(messages.cancel)}
-                </a>
+            <div className="ui grid">
+                <div className="six wide column">
+                    <a className="massive ui button" onClick={onCancel}>
+                        {formatMessage(messages.cancel)}
+                    </a>
+                </div>
+                <div className="ten wide column">
+                    <button
+                        className={`massive pink ui button ${status.submitting
+                        ? 'loading'
+                        : ''}`}
+                        onClick={onSubmit}
+                        disabled={status.submitting}>
+                        {formatMessage(messages.signUp)}
+                    </button>
+                </div>
             </div>
-            <div className="ten wide column">
-                <button
-                    className={`massive pink ui button ${status.submitting
-                    ? 'loading'
-                    : ''}`}
-                    onClick={onSubmit}
-                    disabled={status.submitting}>
-                    {formatMessage(messages.signUp)}
-                </button>
-            </div>
         </div>
-    </div>
-);
+    )
+};
 
 export default injectIntl(AdditionalForm);
