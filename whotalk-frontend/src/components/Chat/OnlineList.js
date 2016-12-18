84d9031f01aa9a44427a7d5d5c119f7cec3f577b
velopert <public.velopert@gmail.com>
2016-12-18 11:40:23 +0900

    fix: userlist animation

--- whotalk-frontend/src/components/Chat/OnlineList.js
+++ whotalk-frontend/src/components/Chat/OnlineList.js
@@ -12,12 +12,17 @@ import React from 'react'
 const OnlineList = () => {
     return (
         <div className="online-list reveal">
-            <div className="title">ONLINE USERS</div>
-            <button className="ui mini black button close">Close</button>
+            <div className="title-bar">
+                <div className="title">ONLINE USERS</div>
+                <button className="ui mini red button circular icon close">
+                    <i className="icon remove"></i>
+                </button>
+            </div>
+            
             <ul>
-                <li>auser</li>
-                <li>anotherUser</li>
-                <li className="anonymous">anonymous</li>
+                <li><i className="user icon"></i>auser</li>
+                <li><i className="user yellow icon"></i>anotherUser</li>
+                <li className="anonymous"><i className="spy icon"></i>anonymous</li>
             </ul>
         </div>
     )
