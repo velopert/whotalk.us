fbd3d0b37525b8160d274dc4528af000099dc4b3
velopert <public.velopert@gmail.com>
2017-01-11 01:02:02 +0900

    social login

--- whotalk-backend/src/passport/google.js
+++ whotalk-backend/src/passport/google.js
@@ -1,5 +1,5 @@
 module.exports = {
   'appID' : '636335359716-a3gq8ukqajgs8k1ffavs0jc5kom2ehlf.apps.googleusercontent.com',
   'appSecret' : process.env.GOOGLE_SECRET,
-  'callbackURL' : 'http://localhost:4000/api/authentication/google/callback'
+  'callbackURL' : 'https://whotalk.us/api/authentication/google/callback'
 }
