d621314cfcb324fd1ec3019b3ee9e15431d964af
velopert <public.velopert@gmail.com>
2017-01-11 01:32:27 +0900

    facebook

--- whotalk-backend/src/passport/fb.js
+++ whotalk-backend/src/passport/fb.js
@@ -1,7 +1,7 @@
 module.exports = {
   'appID' : '2114304178795129',
   'appSecret' : process.env.FACEBOOK_SECRET,
-  'callbackURL' : 'http://localhost:4000/api/authentication/facebook/callback',
+  'callbackURL' : 'https://whotalk.us/api/authentication/facebook/callback',
   'profileFields': ['id', 'name', 'emails', 'friends', 'gender']
 }
 
