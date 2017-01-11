54b9e140edbdc6829c20dfe6e60b02273c1b40f2
velopert <public.velopert@gmail.com>
2017-01-12 03:18:05 +0900

    version update

--- whotalk-backend/src/echo/index.js
+++ whotalk-backend/src/echo/index.js
@@ -6,7 +6,7 @@ import sockets, { connect, disconnect } from './sockets';
 
 
 // initialize server
-const options = { sockjs_url: 'https://cdn.jsdelivr.net/sockjs/1.0.1/sockjs.min.js' };
+const options = { sockjs_url: 'https://cdn.jsdelivr.net/sockjs/1.1.1/sockjs.min.js' };
 const echo = sockjs.createServer(options);
 
 // initialize channel
