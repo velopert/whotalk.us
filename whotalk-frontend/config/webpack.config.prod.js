b7bb283cb03ea4dce23ccc45ab4be9f2844812ca
velopert <public.velopert@gmail.com>
2017-01-12 21:39:25 +0900

    babel-polyfill

--- whotalk-frontend/config/webpack.config.prod.js
+++ whotalk-frontend/config/webpack.config.prod.js
@@ -36,6 +36,7 @@ module.exports = {
   devtool: 'source-map',
   // In production, we only want to load the polyfills and the app code.
   entry: [
+	require.resolve('babel-polyfill'),
     require.resolve('./polyfills'),
     /*
         path.resolve('./semantic/dist/semantic.min.css'),
