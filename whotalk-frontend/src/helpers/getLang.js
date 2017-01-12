a9b46198e4a5f1de33478c847518d4c8b344c154
velopert <public.velopert@gmail.com>
2017-01-13 00:24:42 +0900

    WebView bugFix

--- whotalk-frontend/src/helpers/getLang.js
+++ whotalk-frontend/src/helpers/getLang.js
@@ -1,6 +1,6 @@
 function getLang()
 {
- if (navigator.languages != undefined) 
+ if (navigator.languages != undefined && navigator.languages.length !== 0) 
  return navigator.languages[0]; 
  else 
  return navigator.language;
